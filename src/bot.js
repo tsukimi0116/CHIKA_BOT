d15759452a296a00d29854e1fc825daf490bfbc0
Nathan.hyt <yuting870116@gmail.com>
2022-05-31 23:38:16 +0800

    feat(bot): 更改主架構、增加google sheets API檔案

--- src/bot.js
+++ src/bot.js
@@ -1,7 +1,7 @@
 const fs = require("node:fs");
 const path = require("node:path");
 const { Client, Collection, Intents } = require("discord.js");
-const { token } = require("./config.json");
+const { token } = require("../config.json");
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 module.exports = client;
